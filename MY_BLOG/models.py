26aa71b6572b3dd31ea774131179936d1ff064b0
Dave Augustus <devpandey19924u@gmail.com>
2019-02-26 23:11:52 +0530

    error corrections

--- MY_BLOG/models.py
+++ MY_BLOG/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 GENDER_DEF = (
         ('male','MALE'),
         ('female', 'FEMALE'),
+        ('other', 'Other')
 )
 
 class Profile(models.Model):
-    user        = models.OneToOneField(User, on_delete = models.CASCADE , primary_key = True)
+    user_id     = models.OneToOneField(User, on_delete = models.CASCADE , primary_key = True)
     profession  = models.CharField(max_length = 50)
     gender      = models.CharField(max_length = 10, choices = GENDER_DEF, default = 'Select')
     profile_img = models.ImageField(default = 'default.png', upload_to = 'profile_pics', blank = True, null = True)
@@ -18,24 +19,28 @@ class Profile(models.Model):
 
     def __str__(self):
         # return self.username
-        return '{}'.format(self.user)
+        return '{}'.format(self.user_id)
+
+    class Meta:
+        verbose_name = 'User Details'
+        verbose_name_plural = 'Profile Table'
 
 class Post(models.Model):
-    author          = models.ForeignKey(Profile, on_delete = models.CASCADE)
-    title           = models.CharField(max_length = 50)
+    author_id       = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    title           = models.CharField(max_length = 50, verbose_name='Post Title',)
     about           = models.CharField(max_length = 100)
-    article         = models.TextField(null = True, blank = True)
+    article         = models.TextField(null = True, blank = True, unique = True)
     publish_date    = models.DateTimeField(null = True)
     modified_date   = models.DateTimeField(null = True)
     article_img     = models.ImageField(default = 'default.gpg', upload_to = 'article_pics', blank = True, null = True)
     thumbs_up       = models.IntegerField(null = True)
     thumbs_down     = models.IntegerField(null = True)
-
+    view_count      = models.IntegerField(default = 0)
     def __str__(self):
         return '{}'.format(self.title)
 
 class Comment(models.Model):
-    user     = models.ForeignKey(User, on_delete = models.CASCADE)
+    user_id  = models.ForeignKey(User, on_delete = models.CASCADE)
     article  = models.ManyToManyField(Post)
     comment  = models.TextField()
     liked    = models.BooleanField(default=False)
